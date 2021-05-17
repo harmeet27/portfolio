7061183b88fcbb5dfd9dc1eda265be2a9eeeec69
harmeetkaur27 <harmeet1.kaur@paytm.com>
2021-05-17 23:26:30 +0530

    feat: add name audio

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,18 +6,57 @@ import tech from "../../tech.svg";
 import writer from "../../writer.svg";
 import foodie from "../../foodie.svg";
 import backpack from "../../backpack.svg";
+import speaker from "../../speakerOn.gif";
+import speakerOff from "../../speakerOff.png";
+import audio from "../../MyName.mp3"
 import cv from "../../Harmeet_Kaur_2021.pdf";
 
 class Header extends React.PureComponent {
+  state = {
+    isPlaying: false,
+  };
+
+  audio = new Audio(audio)
+
+  componentDidMount() {
+    this.audio.addEventListener('ended', () => this.setState({ isPlaying: false }));
+  }
+
+  componentWillUnmount() {
+    this.audio.removeEventListener('ended', () => this.setState({ isPlaying: false }));  
+  }
+
+  togglePlay = () => {
+    this.setState({ isPlaying: !this.state.isPlaying }, () => {
+      this.state.isPlaying ? this.audio.play() : this.audio.pause();
+    });
+  }
+
   render() {
     return (
       <div className="header" ref={this.props.aboutRef}>
         <img src={profile} alt="profile" className="profile" />
-        <h1>HI!, I am Harmeet</h1>
-        <a href={cv} target="_blank" rel="noopener noreferrer" download="harmeetkaur.pdf">
-        <button className="cv">Download CV</button>
+        <h1>
+          HI!, I am Harmeet{" "}
+          {
+            <button className="minimal" onClick={this.togglePlay}>
+              {!this.state.isPlaying ? (
+                <img className="speaker" alt="speaker off" src={speakerOff} />
+              ) : (
+                <img className="speaker" alt="speaker" src={speaker} />
+              )}
+            </button>
+          }
+        </h1>
+        <a
+          href={cv}
+          target="_blank"
+          rel="noopener noreferrer"
+          download="harmeetkaur.pdf"
+        >
+          <button className="cv">Download CV</button>
         </a>
-        <br/>
+        <br />
         <span className="tagLine" ref={this.props.projectsRef}>
           <img className="icon" alt="web" src={programmer} /> Web Developer |
           <img className="icon" alt="tech" src={tech} /> Technophile |
