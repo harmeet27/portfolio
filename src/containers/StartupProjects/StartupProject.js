e05653fa162a1fd2fa0a33ff2ce5e93347e2f2f7
amanjadon54 <amanjadon54@gmail.com>
2025-10-28 23:33:34 +0100

    add projects fade

--- src/containers/StartupProjects/StartupProject.js
+++ src/containers/StartupProjects/StartupProject.js
@@ -1,7 +1,7 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import "./StartupProjects.scss";
-import {bigProjects} from "../../portfolio";
-import {Fade} from "react-reveal";
+import { bigProjects } from "../../portfolio";
+import { Fade } from "react-reveal";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function StartupProject() {
@@ -13,28 +13,28 @@ export default function StartupProject() {
     win.focus();
   }
 
-  const {isDark} = useContext(StyleContext);
+  const { isDark } = useContext(StyleContext);
   if (!bigProjects.display) {
     return null;
   }
   return (
-    <Fade bottom duration={1000} distance="20px">
-      <div className="main" id="projects">
-        <div>
-          <h1 className="skills-heading">{bigProjects.title}</h1>
-          <p
-            className={
-              isDark
-                ? "dark-mode project-subtitle"
-                : "subTitle project-subtitle"
-            }
-          >
-            {bigProjects.subtitle}
-          </p>
+    <div className="main" id="projects">
+      <div>
+        <h1 className="skills-heading">{bigProjects.title}</h1>
+        <p
+          className={
+            isDark
+              ? "dark-mode project-subtitle"
+              : "subTitle project-subtitle"
+          }
+        >
+          {bigProjects.subtitle}
+        </p>
 
-          <div className="projects-container">
-            {bigProjects.projects.map((project, i) => {
-              return (
+        <div className="projects-container">
+          {bigProjects.projects.map((project, i) => {
+            return (
+              <Fade key={i} bottom duration={1000} distance="20px">
                 <div
                   key={i}
                   className={
@@ -84,11 +84,11 @@ export default function StartupProject() {
                     ) : null}
                   </div>
                 </div>
-              );
-            })}
-          </div>
+              </Fade>
+            );
+          })}
         </div>
       </div>
-    </Fade>
+    </div>
   );
 }
