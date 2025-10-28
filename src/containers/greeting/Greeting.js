029d296ae98f225de2984fc0353297a071cb4193
amanjadon54 <amanjadon54@gmail.com>
2025-10-28 02:34:07 +0100

    feat: update resume

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,7 +42,7 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume_2025.pdf")}
+                    href={require("./2025HK.pdf")}
                     download="Resume.pdf"
                     className="download-link-button"
                   >
